43e00a643207257529006c2dd5d1e2a27da00ebf
Steven K <stevenks17@gmail.com>
2020-01-21 23:31:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 2, "brown", "grumpy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 4, "male", "black", "sleepy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (3,"Tabitha", 5, "male", "blue", "sleepy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (4,"Wendy", "female", "yellow", "musty", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5,"Sergeant Brown", 7, "male", "brown", "sleepy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6,"Melissa", 7, "male", "brown", "sleepy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7,"Grinch", 7, "male", "brown", "sleepy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (8,NULL, 7, "male", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "brown", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "male", "black", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "male", "blue", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "female", "yellow", "musty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "male", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, "male", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "male", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 7, "male", "brown", "sleepy", 1);
