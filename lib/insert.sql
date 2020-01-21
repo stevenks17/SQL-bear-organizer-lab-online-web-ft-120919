cc7a389d4de21a247ce6d29a78c96477f797ab45
Steven K <stevenks17@gmail.com>
2020-01-21 23:18:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, male, brown, sleepy, yes);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Smokey da BEAR", 7, male, brown, sleepy, yes);
